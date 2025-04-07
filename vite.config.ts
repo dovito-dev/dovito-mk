7de19c2ffc5313aa46c79d950925797a136e2d25
dovito-dev <dev@dovito.com>
2025-04-07 14:55:10 +0000

    Modifications

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/dovito-mk/',
   server: {
     host: "::",
     port: 8080,
